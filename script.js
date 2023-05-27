afde5564721cb8a6977f6206ac103552f55bb881
Султан <carameleclipsimus@gmail.com>
2023-05-27 23:19:08 +0600

    feat: second task of event1 is done

--- script.js
+++ script.js
@@ -11,3 +11,18 @@ btnMsg.addEventListener("click", (e) => {
   e.stopPropagation();
   alert(btnMsg.dataset.text);
 });
+
+// second
+
+/*
+При наведении указателя мыши на "btn-msg", кнопка становится красной;
+когда указатель мыши покидает кнопку, она становится прежнего цвета.
+Цвет менять можно через добавление класса.
+*/
+
+btnMsg.addEventListener("mouseover", (e) => {
+  btnMsg.classList.add("green");
+});
+btnMsg.addEventListener("mouseout", (e) => {
+  btnMsg.classList.remove("green");
+});
